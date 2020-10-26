c7d7e6abed6fbfd7414c2a692434a60abc2037a4
Jon Ganebski <jon.ganebski@gmail.com>
2020-10-27 08:15:55 +0900

    Put helmet on, simple READ.ME.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
@@ -11,7 +10,10 @@ const buildPath = path.join(__dirname, "..", "..", "build");
 
 const app = express();
 
-app.use(helmet());
+// 리액트와 익스프레스를 동시에 올리면 CSP부분을 포기해야 된다. 아니면 리액트 내에 fetch가 전혀 없어야 된다.
+// 리액트 내부의 axios를 전부 백엔드로 옮겨봤지만 Leaflet Map 에서조차 CSP에 걸리는 에러에서 포기했다. ^^
+app.use(helmet({ contentSecurityPolicy: false }));
+
 app.use(express.static(buildPath));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
